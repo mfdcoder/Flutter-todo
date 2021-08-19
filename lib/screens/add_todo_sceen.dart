e16d36c58420a9a3f1e397dc4008ea69c2ec4dd3
mfdcoder <48575544+mfdcoder@users.noreply.github.com>
2021-08-19 23:27:07 +0530

    Update add_todo_sceen.dart

--- lib/screens/add_todo_sceen.dart
+++ lib/screens/add_todo_sceen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './../providers/todo.dart';
+import './../providers/todos_provider.dart';
 
 class AddToDoScreen extends StatefulWidget {
   static const routeName = '/add-todo';
@@ -10,15 +14,44 @@ class AddToDoScreen extends StatefulWidget {
 }
 
 class _AddToDoScreenState extends State<AddToDoScreen> {
+  final _formkey = GlobalKey<FormState>();
+
+  var newToDo = ToDo(
+    id: DateTime.now().toString(),
+    title: 'title',
+    body: 'body',
+    deadline: DateTime.now(),
+  );
+
+  _onSubmit() {
+    print('Submitted');
+    final isValid = _formkey.currentState?.validate();
+    if (isValid != null && !isValid) {
+      return;
+    }
+    _formkey.currentState?.save();
+    print(newToDo.title + ' ' + newToDo.body);
+    final toDoData = Provider.of<ToDosProvider>(context, listen: false);
+    toDoData.addToDo(newToDo);
+    Navigator.of(context).pop();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('New ToDo'),
+          actions: [
+            IconButton(
+              onPressed: () => {_onSubmit()},
+              icon: Icon(Icons.save),
+            )
+          ],
         ),
         body: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Form(
+            key: _formkey,
             child: SingleChildScrollView(
               child: Column(
                 children: [
@@ -26,11 +59,45 @@ class _AddToDoScreenState extends State<AddToDoScreen> {
                     decoration: InputDecoration(labelText: 'Title'),
                     textInputAction: TextInputAction.next,
                     keyboardType: TextInputType.text,
+                    onSaved: (newValue) {
+                      newToDo = ToDo(
+                        id: newToDo.id,
+                        title: newValue as String,
+                        body: newToDo.body,
+                        deadline: newToDo.deadline,
+                      );
+                    },
+                    validator: (value) {
+                      if (value != null && value.isEmpty) {
+                        return 'Title cannot be empty';
+                      } else if (value != null && value.length < 2) {
+                        return 'Min length must be 2';
+                      }
+                      return null;
+                    },
                   ),
                   TextFormField(
                     decoration: InputDecoration(labelText: 'Description'),
-                    textInputAction: TextInputAction.next,
-                    keyboardType: TextInputType.text,
+                    maxLines: 3,
+                    textInputAction: TextInputAction.done,
+                    keyboardType: TextInputType.multiline,
+                    onFieldSubmitted: (_) => {_onSubmit()},
+                    onSaved: (newValue) {
+                      newToDo = ToDo(
+                        id: newToDo.id,
+                        title: newToDo.title,
+                        body: newValue as String,
+                        deadline: newToDo.deadline,
+                      );
+                    },
+                    validator: (value) {
+                      if (value != null && value.isEmpty) {
+                        return 'Description cannot be empty';
+                      } else if (value != null && value.length < 2) {
+                        return 'Min length must be 2';
+                      }
+                      return null;
+                    },
                   )
                 ],
               ),
